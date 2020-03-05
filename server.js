3b1fc690869186f7ad89369e5e1cc75702e43986
Ben Davis <bendavis78@gmail.com>
2020-03-04 22:00:47 -0600

    wip

--- server.js
+++ server.js
@@ -5,32 +5,47 @@ const nunjucks = require('nunjucks');
 const config = require('./config.json');
 const moment = require('moment');
 const debugMiddleware = require('debug-error-middleware').express;
+const NamedRouter = require('named-routes');
 
+config.port = config.port || 3000;
 config.db = config.db || 'db';
 config.locale = config.locale || 'en-US';
-config.port = config.port || 3000;
+config.prefix = config.prefix || '/';
+
 moment.locale(config.locale);
 
-const port = config.port;
 const app = express();
+const router = express.Router();
+const namedRouter = new NamedRouter();
+namedRouter.extendExpress(router);
+
+console.log('Using database', config.db);
 const db = new PouchDB(config.db);
 
 if (config.debug) {
   app.use(debugMiddleware());
 }
 
-console.log(__dirname);
 const template = nunjucks.configure(path.join(__dirname , 'templates'), {
   noCache: true,
   autoescape: true,
   express: app
 });
 
+// log requests
+app.use(function(req, res, next) {
+  console.log(`${req.method} ${req.path}`);
+  next();
+});
+
+// default context
 function getContext(req, res) {
-  return {
-    year: new Date().getFullYear()
-  };
+  return {};
 }
+
+app.locals['url'] = (name, params, method) => {
+  return config.prefix + namedRouter.build(name, params, method);
+};
 
 template.addFilter('date', (date, format) => {
   if (!date) {
@@ -50,8 +65,12 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  const context = getContext();
+router.get('/', 'home', (req, res, next) => {
+  res.render('home.html');
+});
+
+router.get('/entries', 'entries', (req, res, next) => {
+  const context = getContext(req, res);
   let startDate = Date.parse(req.query.from);
   let endDate = Date.parse(req.query.to);
   let startkey = 'entry:';
@@ -68,22 +87,22 @@ app.get('/', (req, res) => {
     endkey: 'entry:\ufff0'
   }).then(result => {
     context.entries = result.rows;
-    res.render('index.html', context)
-  });
+    res.render('entries.html', context)
+  }).catch(error => next(error));
 
 });
 
-app.get('/entries/:timestamp', (req, res) => {
-  const context = getContext();
+router.get('/entries/:timestamp', 'entry', (req, res, next) => {
+  const context = getContext(req, res);
   const timestamp = req.params.timestamp;
   db.get('entry:' + timestamp).then(doc => {
     context.entry = doc;
     res.render('entry.html', context)
-  });
+  }).catch(error => next(error));
 });
 
-app.get('/events', (req, res) => {
-  const context = getContext();
+router.get('/events', 'events', async (req, res, next) => {
+  const context = getContext(req, res);
   let startDate = Date.parse(req.query.from);
   let endDate = Date.parse(req.query.to);
   let startkey = 'entry:';
@@ -93,26 +112,27 @@ app.get('/events', (req, res) => {
   if (!isNaN(endDate)) endkey = 'entry:' + endDate.toString();
 
   // get all sleep entries
-  db.allDocs({
+  await db.allDocs({
     include_docs: true,
     attachments: true,
     startkey: 'event:',
     endkey: 'event:\ufff0'
   }).then(result => {
-    context.entries = result.rows;
+    context.events = result.rows;
     res.render('events.html', context)
-  });
+  }).catch(error => next(error));
 });
 
-app.get('/events/:timestamp', (req, res) => {
-  const context = getContext();
+router.get('/events/:timestamp', 'event', (req, res, next) => {
+  const context = getContext(req, res);
   const timestamp = req.params.timestamp;
   db.get('event:' + timestamp).then(doc => {
     context.event = doc;
     res.render('event.html', context)
-  });
+  }).catch(error => next(error));
 });
 
 app.use('/static', express.static(__dirname + '/static'))
+app.use(config.prefix, router);
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(config.port, () => console.log(`listening on port ${config.port}`));
